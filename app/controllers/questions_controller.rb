b2283473e2e1ff23dd97470204987690076a2541
Diego Larraz <diegolarraz1@gmail.com>
2020-02-17 12:27:47 +0000

    done my stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @user = params[:question]
+    @answer = ''
+    if @user == 'I am going to work'
+      @answer = 'Great!'
+    elsif @user.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
